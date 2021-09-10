4ea780aa3c1636544dc7dccfdbe0b517da886614
Nikolay Karanikolov <karanikolov333@gmail.com>
2021-09-10 13:18:00 +0300

    package

--- capitalize.js
+++
@@ -1,8 +0,0 @@
-const capitalize = (text) => {
-    if (text === '') {
-      return '';
-    }
-    const firstChar = text[0].toUpperCase();
-    const restSubstring = text.slice(1);
-    return `${firstChar}${restSubstring}`;
-  };
